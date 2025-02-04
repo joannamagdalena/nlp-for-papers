c6d287ed254a720b0c5a6b993a1d32152868dbec
Joanna Kaczmarek <joannakaczmarek93@gmail.com>
2025-02-04 17:53:10 +0100

    lemmatization and bigrams

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 library(pdftools)
 library(tm)
 library(tidytext)
+library(tidyverse)
 library(ggplot2)
 library(wordcloud)
 library(wordcloud2)
@@ -9,6 +10,7 @@ library(tokenizers)
 library(hash)
 library(stringr)
 library(dplyr)
+library(textstem)
 
 # loading and pre-processing the pdf file
 pdf_file <- pdf_text("C:/.../x.pdf")
@@ -16,15 +18,25 @@ pdf_file <- tolower(gsub("[\r\n]", " ", paste(pdf_file, collapse=" ")))
 pdf_file <- removePunctuation(pdf_file)
 pdf_file <- removeNumbers(pdf_file)
 
-# tokenization and removing the stop words
-pdf_file_tokenized <- tibble(text = pdf_file) %>% unnest_tokens(word, text)
+# lemmatization
+pdf_file_unlist <- unlist(strsplit(pdf_file, " "))
+pdf_file_lemmatized <- lemmatize_words(pdf_file_unlist)
+
+# tokenization and removing the stop words, with bigrams
+pdf_file_tokenized <- tibble(text = pdf_file_lemmatized) %>% unnest_tokens(word, text)
+pdf_file_tokenized_bigrams <- tibble(text = pdf_file) %>% unnest_tokens(bigram, text, token = "ngrams", n = 2)
 
 stop_words <- stopwords("en") %>% tibble(word = .)
 pdf_file_tokenized <- anti_join(pdf_file_tokenized, stop_words, by = "word")
+pdf_file_tokenized_bigrams <- pdf_file_tokenized_bigrams %>% separate(bigram, c("word1", "word2"), sep = " ")
+pdf_file_tokenized_bigrams <- pdf_file_tokenized_bigrams %>% filter(!word1 %in% stop_words$word) %>% filter(!word2 %in% stop_words$word)
+pdf_file_tokenized_bigrams <- pdf_file_tokenized_bigrams %>% filter(!is.na(word1)) %>% filter(!is.na(word2))
+pdf_file_tokenized_bigrams <- pdf_file_tokenized_bigrams %>% unite(bigram, word1, word2, sep = " ")
 
-# frequency of words
+# frequency of words and bigrams
 word_counts <- pdf_file_tokenized %>% count(word, sort = TRUE)
 word_counts <- mutate(word_counts, freq = n/sum(n))
+bigram_counts <- pdf_file_tokenized_bigrams %>% count(bigram, sort = TRUE)
 
 words_filtered <- word_counts %>% filter(freq > 0.005 & freq <= 0.02)
 
@@ -34,3 +46,5 @@ ggplot(words_filtered, aes(x = reorder(word,-n), y = n)) + geom_bar(stat = "iden
 # word clouds
 wordcloud(words = words_filtered$word, freq = words_filtered$n, min.freq = 0.005, max.words=30, colors = brewer.pal(3, "Set2"))
 wordcloud2(data = words_filtered, size = 0.8, color = "white", backgroundColor = "blue")
+wordcloud2(data = bigram_counts, size = 0.8, color = "white", backgroundColor = "red")
+
