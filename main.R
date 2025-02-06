0e81a0da8e9c7800070de49e5cc95c83f9ddb215
Joanna Kaczmarek <joannakaczmarek93@gmail.com>
2025-02-06 16:28:33 +0100

    bigrams - visualization

--- main.R
+++ main.R
@@ -11,6 +11,8 @@ library(hash)
 library(stringr)
 library(dplyr)
 library(textstem)
+library(igraph)
+library(ggraph)
 
 # loading and pre-processing the pdf file
 pdf_file <- pdf_text("C:/.../x.pdf")
@@ -31,12 +33,12 @@ pdf_file_tokenized <- anti_join(pdf_file_tokenized, stop_words, by = "word")
 pdf_file_tokenized_bigrams <- pdf_file_tokenized_bigrams %>% separate(bigram, c("word1", "word2"), sep = " ")
 pdf_file_tokenized_bigrams <- pdf_file_tokenized_bigrams %>% filter(!word1 %in% stop_words$word) %>% filter(!word2 %in% stop_words$word)
 pdf_file_tokenized_bigrams <- pdf_file_tokenized_bigrams %>% filter(!is.na(word1)) %>% filter(!is.na(word2))
-pdf_file_tokenized_bigrams <- pdf_file_tokenized_bigrams %>% unite(bigram, word1, word2, sep = " ")
+pdf_file_tokenized_bigrams_united <- pdf_file_tokenized_bigrams %>% unite(bigram, word1, word2, sep = " ")
 
 # frequency of words and bigrams
 word_counts <- pdf_file_tokenized %>% count(word, sort = TRUE)
 word_counts <- mutate(word_counts, freq = n/sum(n))
-bigram_counts <- pdf_file_tokenized_bigrams %>% count(bigram, sort = TRUE)
+bigram_counts <- pdf_file_tokenized_bigrams_united %>% count(bigram, sort = TRUE)
 
 words_filtered <- word_counts %>% filter(freq > 0.005 & freq <= 0.02)
 
@@ -48,3 +50,11 @@ wordcloud(words = words_filtered$word, freq = words_filtered$n, min.freq = 0.005
 wordcloud2(data = words_filtered, size = 0.8, color = "white", backgroundColor = "blue")
 wordcloud2(data = bigram_counts, size = 0.8, color = "white", backgroundColor = "red")
 
+# correlations between words
+bigram_counts_sep <- pdf_file_tokenized_bigrams %>% count(word1, word2, sort = TRUE)
+bigram_graph <- bigram_counts_sep %>% filter(n > 10) %>% graph_from_data_frame()
+ggraph(bigram_graph, layout = "fr") + 
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, arrow = grid::arrow(type="closed", length=unit(2, "mm"))) + 
+  geom_node_point() + 
+  geom_node_text(aes(label=name), vjust=1, hjust=1) + 
+  theme_void()
